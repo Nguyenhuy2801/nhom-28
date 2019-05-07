b9e19450392273f35ceabf3ae6b1d7c15ab94ed6
ngocAnh1999 <43126165+ngocAnh1999@users.noreply.github.com>
2019-05-07 22:17:03 +0700

    cập nhật file khởi tạo database và import database

---
+++ Project_28/flask_project/importDatabase.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask_sqlalchemy import SQLAlchemy
+from models import Mon_an, Thanh_phan, Van_hoa, Cach_cb, _mua, Meovaobep, Meovat
+import openpyxl, requests, pymysql
+from api import sqldb
+
+
+path = "/home/ngocanh/Downloads/Database_Congthucnauan.xlsx"
+wb = openpyxl.load_workbook(path)
+
+def checkNull(ten_mon, image, cong_thuc, nguyen_lieu):
+        if(ten_mon is None or image is None or cong_thuc is None or nguyen_lieu is None):
+                print("Some data is null, please check again at ten_mon = {}".format(ten_mon))
+                return False;
+        return True
+
+def importMua():
+        sheet = wb.get_sheet_by_name("Bang_mua_dip_le")
+        max_col = sheet.max_column
+        max_row = sheet.max_row
+        for i in range(0, max_row - 1):
+                cell = sheet.cell(row = i+2, column = 2)
+                mua = _mua()
+                if(mua.query.filter_by(name = name).first() is None):
+                        mua.addMua(cell.value)
+                        print(cell.value + "++++++++++++successfully")
+                
+def importThanh_phan():
+        sheet = wb.get_sheet_by_name("Bang_thanh_phan")
+        max_col = sheet.max_column
+        max_row = sheet.max_row
+        for i in range(0, max_row - 1):
+                cell = sheet.cell(row = i+2, column = 2)
+                thanhphan = Thanh_phan()
+                if(thanhphan.query.filter_by(name = name).first() is None):
+                        thanhphan.addThanhphan(cell.value)
+                        print(cell.value + "++++++++++++successfully")
+
+def importVan_hoa():
+        sheet = wb.get_sheet_by_name("Bang_van_hoa")
+        max_col = sheet.max_column
+        max_row = sheet.max_row
+        for i in range(0, max_row - 1):
+                cell = sheet.cell(row = i+2, column = 2)
+                vanhoa = Van_hoa()
+                if(vanhoa.query.filter_by(name = name).first() is None):
+                        vanhoa.addVanhoa(cell.value)
+                        print(cell.value + "++++++++++++successfully")
+
+def importCachchebien():
+        sheet = wb.get_sheet_by_name("Bang_cach_che_bien")
+        max_col = sheet.max_column
+        max_row = sheet.max_row
+        for i in range(0, max_row - 1):
+                cell = sheet.cell(row = i+2, column = 2)
+                cachchebien = Cach_cb()
+                if(cachchebien.query.filter_by(name = name).first() is None):
+                        cachchebien.addCachchebien(cell.value)
+                        print(cell.value + "++++++++++++successfully")
+
+def importMon_an():
+        sheet = wb.get_sheet_by_name("Bang_mon_an")
+        max_col = sheet.max_column
+        max_row = sheet.max_row
+        print max_col
+        print max_row
+        for i in range(0, max_row - 1):
+                ten_mon = sheet.cell(row = i+2, column = 2).value
+                image = sheet.cell(row = i+2, column = 3).value
+                cong_thuc = sheet.cell(row = i+2, column = 4).value
+                nguyen_lieu = sheet.cell(row = i+2, column = 5).value
+                ma_nl = sheet.cell(row = i+2, column = 6).value
+                ma_cach_cb = sheet.cell(row = i+2, column = 7).value
+                ma_vh = sheet.cell(row = i+2, column = 8).value
+                ma_mua = sheet.cell(row = i+2, column = 9).value
+                video = sheet.cell(row = i+2, column = 10).value
+                
+                monan = Mon_an()
+                # print(monan.query.filter_by(ten_mon=ten_mon).first())
+                if(monan.query.filter_by(ten_mon=ten_mon).first() is None and checkNull(ten_mon, image, cong_thuc, nguyen_lieu)):
+                        print(ten_mon + "++++++++++++successfully")
+                        monan.addMonan(ten_mon, image, cong_thuc, nguyen_lieu, ma_nl, ma_vh, ma_cach_cb, ma_mua, video)
+
+def importMeovaobep():
+        sheet = wb.get_sheet_by_name("Bang_meo_vao_bep")
+        max_col = sheet.max_column
+        max_row = sheet.max_row
+        for i in range(0, max_row - 1):
+                name = sheet.cell(row = i+2, column = 3).value
+                mo_ta = sheet.cell(row = i+2, column = 4).value
+                image = sheet.cell(row = i+2, column = 5).value
+                id_meo = sheet.cell(row = i+2, column = 2).value
+                meovaobep = Meovaobep()
+                if(meovaobep.query.filter_by(name = name).first() is None):
+                        print(name + "++++++++++ successfully")
+                        meovaobep.addMeovaobep(name, mo_ta, image, id_meo)
+                
+def importMeovat():
+        sheet = wb.get_sheet_by_name("Bang_meo_vat")
+        max_col = sheet.max_column
+        max_row = sheet.max_row
+        print(max_col)
+        for i in range(0, max_row - 1):
+                name = sheet.cell(row = i+2, column = 2).value
+                mo_ta = sheet.cell(row = i+2, column = 3).value
+                print(name)
+                meovat = Meovat()
+                if(meovat.query.filter_by(name = name).first() is None):
+                        print(name + "++++++++++ successfully")
+                        meovat.addMeovat(name, mo_ta)
+
+if __name__ == '__main__':
+        print("import data for table Mua:")
+        importMua()
+        print("import data for table Thanh_phan:")
+        importThanh_phan()
+        print("import data for table Van_hoa:")
+        importVan_hoa()
+        print("import data for table Cach_che_bien:")
+        importCachchebien()
+        print("import data for table Mon_an:")
+        importMon_an()
+        print("import data for table Meo_vat:")
+        importMeovat()
+        print("import data for table Meo_vao_bep:")
+        importMeovaobep() 
+        
---
+++ Project_28/flask_project/models.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask_sqlalchemy import SQLAlchemy
+from __main__ import sqldb
+# from api import sqldb
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+
+class Mon_an(sqldb.Model):
+    __tablename__ = 'mon_an'
+
+    ma_mon = sqldb.Column(sqldb.Integer, primary_key = True)
+    ten_mon = sqldb.Column(sqldb.String(255), unique = True, nullable = False)
+    image = sqldb.Column(sqldb.String(255), nullable = False)
+    cong_thuc = sqldb.Column(sqldb.Text, nullable = False)
+    nguyen_lieu = sqldb.Column(sqldb.Text, nullable = False)
+    ma_nl = sqldb.Column(sqldb.Integer, sqldb.ForeignKey('thanh_phan.id'), nullable = True)
+    ma_vh = sqldb.Column(sqldb.Integer, sqldb.ForeignKey('van_hoa.id'), nullable = True)
+    ma_cach_cb = sqldb.Column(sqldb.Integer, sqldb.ForeignKey('cach_cb.id'), nullable = True)
+    ma_mua = sqldb.Column(sqldb.Integer, sqldb.ForeignKey('_mua.id'), nullable = True)
+    video = sqldb.Column(sqldb.String(255), nullable = True)
+
+    def addMonan(self, ten_mon, image, cong_thuc, nguyen_lieu, ma_nl, ma_vh , ma_cach_cb, ma_mua , video):
+        self.ten_mon = ten_mon
+        self.image = image
+        self.cong_thuc = cong_thuc
+        self.nguyen_lieu = nguyen_lieu
+        self.ma_nl = ma_nl
+        self.ma_vh = ma_vh
+        self.ma_cach_cb = ma_cach_cb
+        self.ma_mua = ma_mua
+        self.video = video
+        sqldb.session.add(self)
+        sqldb.session.commit()
+    
+    # def __repr__(self):
+    #     return '%r' % self.ten_mon
+
+
+class Thanh_phan(sqldb.Model):
+    __tablename__ = 'thanh_phan'
+    id = sqldb.Column(sqldb.Integer, primary_key = True)
+    name = sqldb.Column(sqldb.String(255), unique = True, nullable = False)
+
+# func add data to table thanh_phan
+    def addThanhphan(self, excelValue):
+        # thanhphan = Thanh_phan(excelValue)
+        self.name = excelValue
+        sqldb.session.add(self)
+        sqldb.session.commit()
+
+class Van_hoa(sqldb.Model):
+    __tablename__ = 'van_hoa'
+    id = sqldb.Column(sqldb.Integer, primary_key = True)
+    name = sqldb.Column(sqldb.String(255), unique = True, nullable = False)
+
+# func add data to table Van_hoa
+    def addVanhoa(self, excelValue):
+        self.name = excelValue
+        sqldb.session.add(self)
+        sqldb.session.commit()
+
+class _mua(sqldb.Model):
+    __tablename__ = '_mua'
+    id = sqldb.Column(sqldb.Integer, primary_key = True)
+    name = sqldb.Column(sqldb.String(255), unique = True, nullable = False)
+
+
+# func add data to table _mua
+    def addMua(self, excelValue):
+        self.name = excelValue
+        sqldb.session.add(self)
+        sqldb.session.commit()
+
+class Cach_cb(sqldb.Model):
+    __tablename__ = 'cach_cb'
+    id = sqldb.Column(sqldb.Integer, primary_key = True)
+    name = sqldb.Column(sqldb.String(255), unique = True, nullable = False)
+
+# func add data to table Cach_cb
+    def addCachchebien(self, excelValue):
+        self.name = excelValue
+        sqldb.session.add(self)
+        sqldb.session.commit()
+
+class Meovaobep(sqldb.Model):
+    __tablename__ = 'meovaobep'
+    id = sqldb.Column(sqldb.Integer, primary_key = True)
+    name = sqldb.Column(sqldb.String(255), unique = True, nullable = False)
+    mo_ta = sqldb.Column(sqldb.Text, nullable = False)
+    image = sqldb.Column(sqldb.String(255))
+    id_meo = sqldb.Column(sqldb.Integer, sqldb.ForeignKey('meovat.id'), nullable = True)
+
+    def addMeovaobep(self, name, mo_ta, image, id_meo):
+        self.name = name
+        self.mo_ta = mo_ta
+        self.image = image
+        self.id_meo = id_meo
+        sqldb.session.add(self)
+        sqldb.session.commit()
+
+class Meovat(sqldb.Model):
+    __tablename__ = 'meovat'
+    id = sqldb.Column(sqldb.Integer, primary_key = True)
+    name = sqldb.Column(sqldb.String(255), unique = True, nullable = False)
+    mo_ta = sqldb.Column(sqldb.Text, nullable = False)
+
+    def addMeovat(self, name, mo_ta):
+        self.name = name
+        self.mo_ta = mo_ta
+        sqldb.session.add(self)
+        sqldb.session.commit()
+
+
+if __name__ == '__main__':
+
+    sqldb.create_all()
+    # results = Mon_an.query.with_entities(Mon_an.ten_mon, Mon_an.image).\
+    #     order_by(Mon_an.ma_mon.desc()).limit(10).all()
+    # for result in results:
+    #     print(result[1])
+
