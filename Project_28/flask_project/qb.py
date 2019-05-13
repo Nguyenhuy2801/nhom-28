693f4a40841948257a9e01274f446f661f0d6d4a
ngocAnh1999 <43126165+ngocAnh1999@users.noreply.github.com>
2019-05-13 23:14:48 +0700

    Update qb.py

--- Project_28/flask_project/qb.py
+++ Project_28/flask_project/qb.py
@@ -2,12 +2,20 @@
 # -*- coding: utf-8 -*-
 import pymysql
 
-host = "localhost"
-user = "ngocanh"
-password = "nao123456"
+host = "foodforfamilyf3.mysql.pythonanywhere-services.com"
+user = "foodforfamilyf3"
+password = "ngocanh2512"
 dbname = "fshare"
 
 def connection():
-    
+
     db = pymysql.connect(host, user, password, dbname)
     return db
+
+if __name__  == '__main__':
+    cursor = connection().cursor()
+    query = "select ten_mon from mon_an limit 5"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
